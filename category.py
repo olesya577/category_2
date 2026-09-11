236f4f09953494baa76ccba8782cd2b19be65e1d
Olesya <flomaster597@gmail.com>
2026-09-11 20:25:34 +0500

    hg

---
+++ category.py
@@ -0,0 +1,18 @@
+class Category:
+    """Класс, который хранит информацию о категориях продуктов"""
+    name: str
+    description: str
+    products: list
+    product_count: int
+    category_count: int
+    product_count = 0
+    category_count = 0
+
+    def __init__(self, name, description, products):
+        """Метод инициализации экземпляра класса"""
+        self.name = name
+        self.description = description
+        self.products = products if products else []
+        Category.category_count += 1
+        Category.product_count += len(products)
+
